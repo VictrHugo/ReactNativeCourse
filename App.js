a7292026913d4af8e6e14162174863849faabee0
Victor Hugo Chimenez <victor.hugo@gringo.com.vc>
2021-10-12 17:34:35 -0300

    sec3: Flat List

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import ComponentsScreen from "./src/screens/ComponentsScreen";
-import HomeScreen from "./src/screens/HomeScreen";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponentsScreen
+    Components: ComponentsScreen,
+    List: ListScreen,
   },
   {
-    initialRouteName: "Components",
+    initialRouteName: 'List',
     defaultNavigationOptions: {
-      title: "App",
+      title: 'App',
     },
   }
 );
