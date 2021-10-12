e7f54a9b9ad289767f4054470d8ea730746601d1
Victor Hugo Chimenez <victor.hugo@gringo.com.vc>
2021-10-12 17:46:19 -0300

    sec4: Button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello!</Text>;
-};
+  return (<View>
+    <Text style={styles.text}>Hello!</Text>
+    <Button 
+      onPress = {() => console.log('Button pressed!')}
+      title = 'Go to Componets Demo' 
+      />
+    </View>)
+  
+}
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
   }
-});
+})
 
 export default HomeScreen;
